d5662d93313a574def4133b1b3bb0538c3bae812
papercut <papercut@172.17.60.155>
2019-08-20 16:34:22 +0700

    20/8/62

---
+++ src/chap3/TestOuterB.java
@@ -0,0 +1,15 @@
+package chap3;
+//(2)
+class OuterB{
+    
+    //(3)
+    class MemberInner{
+    
+    }//end class StaticInner
+    
+}//end class OuterA
+
+//(1)
+public class TestOuterB {
+    
+}//end class TestOuterA
