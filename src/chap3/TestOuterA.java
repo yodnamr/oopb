01d95c54aa42ed8d930a87cb65ae0ddce716285a
papercut <papercut@172.17.60.155>
2019-08-20 15:55:33 +0700

    20/8/62

---
+++ src/chap3/TestOuterA.java
@@ -0,0 +1,15 @@
+package chap3;
+//(2)
+class OuterA{
+    
+    //(3)
+    static class StaticInner{
+    
+    }//end class StaticInner
+    
+}//end class OuterA
+
+//(1)
+public class TestOuterA {
+    
+}//end class TestOuterA
